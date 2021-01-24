51835eb43c16eb5654959d99c9029e94dc113980
shivaraj shooru <shivarajshooru@gmail.com>
2021-01-24 20:39:41 +0530

    done203

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -9,9 +9,53 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setRenderBall(false);
+   updateXY(0, 0);
+  };
+  const start = () => {
+    setRenderBall(true);
+  };
+  const renderChoice = () => {
+    return renderBall ? (
+      <div className="ball" style={{
+        position: 'absolute',
+        left: ballPosition.left,
+        top: ballPosition.top
+      }}></div>
+    ) : (
+      <button onClick={start} className="start">
+        start
+      </button>
+    );
+  };
 
+  const updateXY = (nx,ny) => {
+    setX(nx);
+    setY(ny);
+    setBallPosition({
+      left: nx + 'px',
+      top: ny + 'px'
+    });
+  }
+
+  useEffect(() => {
+    const keyListener = (evt) => {
+      if(renderBall) {
+        if (evt.keyCode === 37) {
+          updateXY(x - 5, y);
+        } else if (evt.keyCode === 38) {
+          updateXY(x, y - 5);
+        } else if (evt.keyCode === 39) {
+          updateXY(x + 5, y);
+        } else if (evt.keyCode === 40) {
+          updateXY(x , y + 5);
+      }
+    }
+  };
+  document.addEventListener("keydown", keyListener);
+ return () => document.removeEventListener("keydown", keyListener);
+});
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
